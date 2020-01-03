ff960fef050abf7a866cd427e1995ad7ee001b48
Junaid Rahim <junaidrahim8d@gmail.com>
2020-01-03 15:02:31 +0530

    prefix to postfix

--- Data Structures/Stack/standard_problems/3.cc
+++ Data Structures/Stack/standard_problems/3.cc
@@ -7,9 +7,36 @@
 
 using namespace std;
 
+string prefix_to_postfix(string input) {
+    stack<string> st;
+    string answer;
+
+    for (auto c = input.end()-1; c>=input.begin(); --c) {
+        if(isalpha(*c)){
+            st.push(string{*c});
+        } 
+        else { // if its an op
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+
+            string s = t1 + t2 + string{*c};
+            st.push(s);
+        }
+    }
+
+    while (!st.empty()) {
+        answer += st.top();
+        st.pop();
+    }
+
+    return answer;
+}
 
 
 int main(){
+    cout << prefix_to_postfix("*+AB-CD") << endl;
 
     return 0;
 }
